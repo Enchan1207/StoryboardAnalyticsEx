b09aaafa0ddc12bb925ae4e71b3dfbc8804ab755
Enchan <enchant0130@gmail.com>
2021-03-26 22:47:50 +0900

    [Update] ViewController listup

--- Sources/StoryboardAnalyticsEx/main.swift
+++ Sources/StoryboardAnalyticsEx/main.swift
@@ -4,30 +4,43 @@
 import Foundation
 
 // Resources/ からXMLファイルを読み込む
-print("load Storyboard.xml...")
-let XMLFileName = "sample"
+let XMLFileName = "Storyboard"
+print("load \(XMLFileName).xml...")
 guard let XMLFileURL = Bundle.module.url(forResource: XMLFileName, withExtension: "xml"),
       let XMLFileData = try? Data(contentsOf: XMLFileURL) else {exit(EXIT_FAILURE)}
 
 // XMLパーサに通す
-let semaphore = DispatchSemaphore(value: 0)
 print("parse...")
+let semaphore = DispatchSemaphore(value: 0)
+var root: Element?
 let parser: XMLDOMParser = XMLDOMParser(data: XMLFileData)
 parser.parse { (node) in
-    print(node)
+    root = node as? Element
     semaphore.signal()
 } failure: { (error) in
     print(error)
     semaphore.signal()
 }
 semaphore.wait()
+guard let root = root else{exit(EXIT_FAILURE)}
 
-// webサイトから持ってきてパース
-//let task = URLSession.shared.dataTask(with: URL(string: "https://example.com")!) { (data, response, error) in
-//    print(response)
-//    semaphore.signal()
-//}
-//task.resume()
-//semaphore.wait()
+// SBに含まれるConrtollerを抽出
+guard let scenes = root.getElementsBy(tagName: "scenes").first?.getElementsBy(tagName: "scene") else {exit(EXIT_FAILURE)}
+for scene in scenes{
+    print("-------------------------")
+    
+    print("scene (id: \(scene.attributes["sceneID"]!))")
+    let objects = scene.getElementsBy(tagName: "objects").first!.getElements(by: "sceneMemberID", value: "viewController")
+    for object in objects{
+        print("  \(object.tagName) (id: \(object.attributes["id"]!))")
+        
+        // segue取得
+        guard let segues = object.getElementsBy(tagName: "connections").first?.getElementsBy(tagName: "segue") else {continue}
+        print("  segue:")
+        for segue in segues {
+            print("    kind: \(segue.attributes["kind"] ?? "none") destination: \(segue.attributes["destination"] ?? "none") (id: \(object.attributes["id"]!)) ")
+        }
+    }
+}
 
 exit(EXIT_SUCCESS)
